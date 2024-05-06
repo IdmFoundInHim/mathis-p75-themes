7bcdde00a7b82e53da2099019ebde707f28cd36e
Isaac Myhal <idmfoundinhim@gmail.com>
2024-05-06 17:21:53 -0400

    Switch suspend to hibernate

--- .config/wofi/scripts/powermenu.sh
+++ .config/wofi/scripts/powermenu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-entries="⭮ Reboot\n⏻ Shutdown\n⇠ Logout\n⏾ Suspend"
+entries="⭮ Reboot\n⏻ Shutdown\n⇠ Logout\n⏾ Hibernate"
 
 selected=$(echo -e $entries|wofi --width 250 --height 260 --dmenu $2 --style ~/.config/wofi/themes/$1.css --hide_search=true --hide-scroll --cache-file /dev/null | awk '{print tolower($2)}')
 
 case $selected in
   logout)
     exec hyprctl dispatch exit NOW;;
-  suspend)
-    exec systemctl suspend;;
+  hibernate)
+    exec systemctl hibernate;;
   reboot)
     exec systemctl reboot;;
   shutdown)
